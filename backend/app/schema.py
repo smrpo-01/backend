3f741070859e04696c173a896a92ac7510617003
marok39 <rok.marinsek94@gmail.com>
2018-03-24 12:37:43 +0100

    Fuj to, stran!

--- backend/app/schema.py
+++ backend/app/schema.py
@@ -95,26 +95,6 @@ class Query(graphene.ObjectType):
             return info.context.user
         return None
 
-"""
-class PersonInput(graphene.InputObjectType):
-    name = graphene.String(required=True)
-    age = graphene.Int(required=True)
-
-class CreatePerson(graphene.Mutation):
-    class Arguments:
-        person_data = PersonInput(required=True)
-
-    person = graphene.Field(Person)
-
-    @staticmethod
-    def mutate(root, info, person_data=None):
-        person = Person(
-            name=person_data.name,
-            age=person_data.age
-        )
-        return CreatePerson(person=person)
-"""
-
 
 class CreateUserInput(graphene.InputObjectType):
     email = graphene.String(required=True)

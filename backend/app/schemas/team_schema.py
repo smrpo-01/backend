3e215681e01ab120efec3c6b4a3fd9bf682cc1f9
Jan Šubelj <jan.subelj010@gmail.com>
2018-03-25 21:44:44 +0200

    dodal team edit

--- backend/app/schemas/team_schema.py
+++ backend/app/schemas/team_schema.py
@@ -103,7 +103,7 @@ class CreateTeam(graphene.Mutation):
                 user_team.roles.add(models.TeamRole.objects.get(id=role))
             user_team.save()
 
-            user_team_log = models.UserTeamLog(action="User added to team", userteam_id=user_team)
+            user_team_log = models.UserTeamLog(action="User added to team", userteam=user_team)
             user_team_log.save()
 
         return CreateTeam(team=team, ok=True)
@@ -140,57 +140,118 @@ class EditTeam(graphene.Mutation):
 
     @staticmethod
     def mutate(root, info, team_data=None):
-        #if team_data.km_id is not None:
-         #   print("KUL")
-
         team = models.Team.objects.get(id=team_data.team_id)
         team_members = []
         team_members_id = []
+        old_member_roles = {}
+        newly_added_users_ids = []
         if team_data.members is not None:
             for member in team_data.members:
                 try:
                     # če je že v ekipi ga povlečemo vn
                     team_member = models.UserTeam.objects.get(team=team, member=models.User.objects.get(id=member.id))
-                    team_member.roles.all().delete()
-                    for role in member.roles:
-                        print("ROLE: " + str(role))
-                        team_member.roles.add(models.TeamRole.objects.get(id=2))
+                    old_roles = list(team_member.roles.all())
+                    team_member.roles.through.objects.filter(userteam=team_member).delete()
 
+                    for role in member.roles:
+                        team_member.roles.add(models.TeamRole.objects.get(id=role))
+
+                    old_member_roles[team_member] = old_roles
                     team_members.append(team_member)
                     team_members_id.append(member.id)
+                    
                 except ObjectDoesNotExist:
                     # nova dodaja v ekipo
                     team_member = models.UserTeam(member=models.User.objects.get(id=member.id), team=team)
+                    team_member.save()
+                    newly_added_users_ids.append(team_member.id)
                     for role in member.roles:
                         team_member.roles.add(models.TeamRole.objects.get(id=role))
                     team_members.append(team_member)
                     team_members_id.append(member.id)
-        print("do sm")
+
         # get rest of team members
         for team_member in list(models.UserTeam.objects.filter(team=team)):
             if team_member.member.id not in team_members_id:
                 team_members.append(team_member)
 
-        print(team_members)
+        no_po = 0
+        no_km = 0
+        no_dev = 0
+        error = None
+        for user in team_members:
+            u = models.User.objects.get(id=user.member.id)
+            if u is None:
+                error = 'User: %d, does not exist' % user.member.id
+            # preveri če je user zmožen ush assignanih team_rolov
+            user_roles = list(u.roles.filter())
+            team_roles = []
+            new_user_roles = [role.id for role in user.roles.all()]
+            for team_role in new_user_roles:
+                team_roles.append(models.UserRole.objects.get(id=team_role))
+                if team_role == 2:
+                    if team_data.po_id != u.id:
+                        if team_data.po_id is None:
+                            error = 'PO role given but not correctly assigned to team'
+                        else:
+                            error = 'PO assigned: %d, is not the same as: %d' % (team_data.po_id, u.id)
+                    no_po += 1
+                elif team_role == 3:
+                    if team_data.km_id != u.id:
+                        if team_data.km_id is None:
+                            error = 'KS role given but not correctly assigned to team'
+                        else:
+                            error = 'KM assigned: %d, is not the same as: %d' % (team_data.km_id, u.id)
+                    no_km += 1
+                elif team_role == 4:
+                    no_dev += 1
+                else:
+                    error = 'No such role: %d, UserId: %d' % (team_role,  user.member.id)
 
+            if not all(e in user_roles for e in team_roles):
+                error = 'User: %d, can\'t do assigned jobs' % user.member.id
 
+            # preveri da ni naenkrat km in po
+            if (models.UserRole.objects.get(id=3) in team_roles) and (models.UserRole.objects.get(id=2) in team_roles):
+                error = "Team member cannot be KM and PO ath the same time"
 
+        if (no_dev == 0) or (no_km != 1) or (no_po != 1):
+            error = 'Insufficient number of team members: noPO: %d, noKM: %d, noDev: %d' % (no_po, no_km, no_dev)
 
-        #team_members_u = []
-        #for member in team_members:
-        #    team_members_u.append(member.member)
-        #print(team_members_u)
-        #for user in team_data.members:
-         #   if user in team.members
+        if error is not None:
+            # revert back to old roles
+            for user in team_members:
+                try:
+                    old_roles = old_member_roles[user]
+                    user.roles.through.objects.filter(userteam=team_member).delete()
+                    for role in old_roles:
+                        user.roles.add(role)
+                    user.save()
+                except KeyError:
+                    pass
 
+            for user_id in newly_added_users_ids:
+                models.UserTeam.objects.filter(id=user_id).delete()
 
-        #for user in team_data.members:
+            raise GraphQLError(error)
 
+        # no errors save everything and add to log
+        for user in team_members:
+            user.save()
 
+        for user_id in newly_added_users_ids:
+            user_team_log = models.UserTeamLog(action="User added to team", userteam=models.UserTeam.objects.get(id=user_id))
+            user_team_log.save()
 
+        if team_data.po_id is not None:
+            team.product_owner = models.User.objects.get(id=team_data.po_id)
+            team.save()
 
+        if team_data.km_id is not None:
+            team.kanban_master = models.User.objects.get(id=team_data.km_id)
+            team.save()
 
-        return EditTeam(team=None, ok=True)
+        return EditTeam(team=team, ok=True)
 
 
 class TeamQueries(graphene.ObjectType):
